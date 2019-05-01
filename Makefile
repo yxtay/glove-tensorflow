581da883867c3981199646049c51977fdb0cdbc3
yuxuan.tay <wyextay@gmail.com>
2019-05-01 17:54:47 +0800

    add makefile

---
+++ Makefile
@@ -0,0 +1,48 @@
+.PHONY: data
+data:
+	python -m src.data.text8
+
+.PHONY: docker-data
+docker-data:
+	docker run --rm -w=/home \
+	  --mount type=bind,source=$(pwd),target=/home \
+	  continuumio/anaconda3:5.3.0 \
+	  python -m src.data.text8
+
+.PHONY: train
+train:
+	python -m trainer.glove
+
+.PHONY: docker-train
+docker-train:
+	docker run --rm -w=/home \
+	  --mount type=bind,source=$(pwd),target=/home \
+	  tensorflow/tensorflow:1.13.1-py3 \
+	  python -m trainer.glove
+
+.PHONY: tensorboard
+tensorboard:
+	tensorboard --logdir checkpoints/
+
+.PHONY: docker-tensorboard
+docker-tensorboard:
+	docker run --rm -w=/home -p 6006:6006 \
+	  --mount type=bind,source=$(pwd),target=/home \
+	  tensorflow/tensorflow:1.13.1-py3 \
+	  tensorboard --logdir checkpoints/
+
+.PHONY: serving
+serving:
+	docker run --rm -p 8500:8500 -p 8501:8501 \
+	  --mount type=bind,source=$(shell pwd)/checkpoints/glove/export/exporter,target=/models/glove \
+	  -e MODEL_NAME=glove -t tensorflow/serving:1.12.0
+
+.PHONY: query
+query:
+	curl -X POST \
+	  http://localhost:8501/v1/models/glove:predict \
+	  -d '{"instances": [{"row_token": "man", "col_token": "man"}]}'
+
+.PHONY: embeddings
+embeddings:
+	python -m src.model.export_embeddings
---
+++ src/model/export_embeddings.py
@@ -0,0 +1,73 @@
+import json
+import os
+import sys
+from argparse import ArgumentParser
+
+import tensorflow as tf
+
+from src.logger import get_logger
+from trainer.glove import get_estimator, get_predict_input_fn
+
+logger = get_logger(__name__)
+
+
+def format_predictions(predictions):
+    embeddings = {}
+    for instance in predictions:
+        # need to convert byte to string
+        # and remove numpy types
+        item_id = instance["row_id"].decode()
+        row_embed = instance["row_embed"].tolist()
+        col_embed = instance["col_embed"].tolist()
+
+        # add entry only if valid predictions
+        if item_id != "<UNK>":
+            instance_embedding = {"item_id": item_id, "row_embed": row_embed, "col_embed": col_embed}
+            embeddings[item_id] = instance_embedding
+    logger.info("embedding dict size: %s.", len(embeddings))
+    return embeddings
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--job-dir",
+        default="checkpoints/glove",
+        help="job directory (default: %(default)s)"
+    )
+    parser.add_argument(
+        "--embeddings-json",
+        default="checkpoints/embeddings/embeddings.json",
+        help="path to the embeddings json (default: %(default)s)"
+    )
+    args = parser.parse_args()
+    logger.info("call: %s.", " ".join(sys.argv))
+    logger.info("ArgumentParser: %s.", args)
+
+    try:
+        job_dir = args.job_dir
+        embeddings_json = args.embeddings_json
+
+        # load params
+        params_json = os.path.join(job_dir, "params.json")
+        with tf.io.gfile.GFile(params_json) as f:
+            params = json.load(f, parse_float=False)
+        logger.info("params loaded: %s.", params_json)
+
+        # estimator
+        estimator = get_estimator(job_dir, params)
+        logger.info("loading checkpoint: %s.", job_dir)
+
+        # get predictions
+        predict_input_fn = get_predict_input_fn(params["mappings"][params["field_names"]["row_id"]])
+        predictions = estimator.predict(predict_input_fn)
+        embeddings = format_predictions(predictions)
+
+        # save predictions
+        with tf.io.gfile.GFile(embeddings_json, "w") as f:
+            json.dump(embeddings, f, indent=2)
+        logger.info("embeddings_json saved: %s.", embeddings_json)
+
+    except Exception as e:
+        logger.exception(e)
+        raise e
